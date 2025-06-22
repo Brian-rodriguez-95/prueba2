2f0939be0717755f3517101cecdd9f6444d32f9a
Brian Rodriguez <rodriguezbraii@gmail.com>
2025-06-22 16:43:10 -0300

    creacion de app y prueba de respuesta

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # prueba2
+
+crear aplicacion 
+
+    python manage.py startapp core
+
+despues hay que registrarla en settings
+
+
+
---
+++ proyecto1/myapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def saludar (request):
+    return HttpResponse("hola desde django") #clase httpResponse
+
+
+# Create your views here.
+#def index(request):
+#    context = {"mensaje": "bienvenidos a mi aplicacion django"}
+#    return render(request, "myapp/index.html", context)
+
+#un context (en este caso "mensaje")es un diccionario en que la clave es lo que se le pasa al html
+# y el valor en este caso ("bienvenidos a mi ....") lo uqe quier oque se pase al html como un contenido 
+# return render(request(primercodigo), "myapp/index.html"(ubicacion), context(diccionario))
+
+
+#el "mensaje" que esta en context es el <p>mensaje</p> que esta en index.html
+
+
+# se necesita index, para cuando haga una solicitud al sitio web, se recibe como repuesta lo que tengamos en la funsion index(html) 
